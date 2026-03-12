22d579400e1065602eb83f68a7e7888856aae309
kosssst <medvedtskyi.kostya@gmail.com>
2026-03-12 13:21:58 +0200

    added version tag

--- src/components/headers/MainHeader.tsx
+++ src/components/headers/MainHeader.tsx
@@ -1,26 +1,30 @@
 'use client';
 
-import { Container, Image } from '@mantine/core';
+import { Container, Group, Image } from '@mantine/core';
 import { UserCardSmall } from '@/components/containers/UserCardSmall';
 import { UserProps } from '@/types/props.types';
 import classes from '@/styles/MainHeader.module.css';
 import Link from 'next/link';
+import { VersionTag } from '@/components/containers/VersionTag';
 
 export function MainHeader({ user }: UserProps) {
 	return (
 		<header className={classes.header}>
 			<Container size="lg" className={classes.container}>
 				<div className={classes.inner}>
-					<Link href="/" className={classes.logoLink}>
-						<Image
-							src={'/full-logo.png'}
-							alt="logo"
-							className={classes.fullLogo}
-							h={40}
-							w="auto"
-							fit="contain"
-						/>
-					</Link>
+					<Group>
+						<Link href="/" className={classes.logoLink}>
+							<Image
+								src={'/full-logo.png'}
+								alt="logo"
+								className={classes.fullLogo}
+								h={40}
+								w="auto"
+								fit="contain"
+							/>
+						</Link>
+						<VersionTag />
+					</Group>
 					<Link href="/profile" className={classes.profileLink}>
 						<UserCardSmall user={user} />
 					</Link>
