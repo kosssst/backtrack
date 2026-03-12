f6867803c971a596e1463a10f7b01ae24cd91beb
kosssst <medvedtskyi.kostya@gmail.com>
2026-03-12 11:34:24 +0200

    added sign out button

--- src/components/containers/UserCard.tsx
+++ src/components/containers/UserCard.tsx
@@ -1,7 +1,30 @@
-import { Avatar, Card, Stack, Text, Title } from '@mantine/core';
+'use client';
+
+import { Avatar, Button, Card, Stack, Text, Title } from '@mantine/core';
 import { UserProps } from '@/types/props.types';
+import { authClient } from '@/lib/auth/auth-client';
+import { useRouter } from 'next/navigation';
+import { notifications } from '@mantine/notifications';
 
 export function UserCard({ user }: UserProps) {
+	const router = useRouter();
+	const handleLogout = async () => {
+		await authClient.signOut({
+			fetchOptions: {
+				onSuccess: () => {
+					router.push('/login');
+				},
+				onError: () => {
+					notifications.show({
+						title: 'Failure',
+						message: 'Something went wrong',
+						color: 'red',
+					});
+				},
+			},
+		});
+	};
+
 	return (
 		<Card withBorder radius="lg" p="xl">
 			<Stack gap="md" align="center">
@@ -14,6 +37,9 @@ export function UserCard({ user }: UserProps) {
 						{user.email}
 					</Text>
 				</Stack>
+				<Button type="submit" onClick={handleLogout} radius="md">
+					Sign Out
+				</Button>
 			</Stack>
 		</Card>
 	);
